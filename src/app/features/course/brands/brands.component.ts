ca38f19346afd59f55d419d9664d94039db56d34
shivam dubey <shivamdubey781@outlook.com>
2025-08-23 15:19:25 +0530

    temp commit

---
+++ src/app/features/course/brands/brands.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { LandingPageComponent } from '../landing-page/landing-page.component';
+
+@Component({
+  selector: 'app-brands',
+  standalone: true,
+  imports: [],
+  templateUrl: './brands.component.html',
+  styleUrl: './brands.component.scss'
+})
+export class BrandsComponent {
+
+}
