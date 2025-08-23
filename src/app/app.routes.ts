6835a23b64989b242ad90e52f6de8729f67c15e9
Siddharth Dwivedi <siddharth.dwivedi@ecolab.com>
2025-08-23 12:15:07 +0530

    boiler plate code added for the initial code base

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
+import { provideRouter, Routes } from '@angular/router';
+import { LandingPageComponent } from './features/course/landing-page/landing-page.component';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'landing-page', pathMatch: 'full' }, // Default route
+    { path: 'landing-page', component: LandingPageComponent },  // Route for landing-page
+];
+
+bootstrapApplication(AppComponent, {
+    providers: [provideRouter(routes)]
+  });
+  
