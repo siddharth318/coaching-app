48bf9ad01c90900dda33ed56f7a11f877797d211
shivam dubey <shivamdubey781@outlook.com>
2025-08-24 11:01:57 +0530

    brand svg added

--- src/app/features/course/landing-page/landing-page.component.ts
+++ src/app/features/course/landing-page/landing-page.component.ts
@@ -10,5 +10,42 @@ import { BrandsComponent } from '../brands/brands.component';
 })
 export class LandingPageComponent {
 
+  // testimonial-card.component.ts
+
+cards = [
+  {
+    name: 'Faraz Ahmad',
+    course: 'Advanced JavaScript',
+    rating: 5,
+    text: 'I was a scrub before I discovered ui.dev. I used their Advanced JavaScript course to prepare for interviews and landed a job at Netflix. All their courses are bangers.',
+    color: '#ED8FAA', // pink
+    image: 'assets/pic1.jpg' // replace with photo path
+  },
+  {
+    name: 'Name2',
+    course: 'React Hooks',
+    rating: 5,
+    text: 'Another awesome testimonial text.',
+    color: '#FFC300', // yellow
+    image: 'assets/pic2.jpg'
+  },
+  {
+    name: 'Name3',
+    course: 'TypeScript',
+    rating: 4,
+    text: 'Yet another testimonial.',
+    color: '#46C47C', // green
+    image: 'assets/pic3.jpg'
+  },
+];
+
+  
+getStars(rating: number): number[] {
+  return Array(rating);
+}
+shuffleCards() {
+  this.cards = [...this.cards].sort(() => Math.random() - 0.5);
+}
+
   
 }
